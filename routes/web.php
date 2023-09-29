ae1640a5fbf918ca851de712492977903d47dcbb
nasar8202 <nasar.ullah@oip.com.pk>
2023-09-30 03:53:03 +0500

    tttt

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-// tssdd
+// tssdddfdfdf
 Route::get('/', function () {
     return view('welcome');
 });
